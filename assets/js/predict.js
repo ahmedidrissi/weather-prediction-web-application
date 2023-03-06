b68888471921683f52945814a2518cedbd7e90c1
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2023-03-06 18:30:28 +0100

    update predict.js

--- assets/js/predict.js
+++ assets/js/predict.js
@@ -1,30 +1,40 @@
-classes = {
-            0: 'dew',
-            1: 'fogsmog',
-            2: 'frost',
-            3: 'glaze',
-            4: 'hail',
-            5: 'lightning',
-            6: 'rain',
-            7: 'rainbow',
-            8: 'rime',
-            9: 'sandstorm',
-            10: 'snow'
-        };
+const classes = {
+        0: 'dew',
+        1: 'fogsmog',
+        2: 'frost',
+        3: 'glaze',
+        4: 'hail',
+        5: 'lightning',
+        6: 'rain',
+        7: 'rainbow',
+        8: 'rime',
+        9: 'sandstorm',
+        10: 'snow'
+    };
+    
+var model;
+
+function loadFile(event) {
+    var image = document.getElementById('preview');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function loadModel() {
+    
+}
 
 async function predictWeather() {
     var result = document.getElementById('result');
-    result.innerHTML = "Processing image ...";
-    const model = await tf.loadLayersModel('./assets/model/model.json');
-    //model.summary();
+    result.innerHTML = "Predicting ...";    
+    model = await tf.loadLayersModel('./assets/model/model.json');
     var pred = model.predict(preprocess()).dataSync();
     var index = pred.indexOf(Math.max(...pred));
-    result.innerHTML = classes[index];
-}
+    result.innerHTML = "Output : " + classes[index];
+};
 
 function preprocess()
 {
-    var img = document.getElementById('output');
+    var img = document.getElementById('preview');
     //convert the image data to a tensor 
     let tensor = tf.browser.fromPixels(img);
     //resize to 256 X 256
@@ -34,5 +44,6 @@ function preprocess()
     const normalized = tf.scalar(1.0).sub(resized.div(offset));
     //We add a dimension to get a batch shape 
     const batched = normalized.expandDims(0);
+
     return batched
-}
+};
